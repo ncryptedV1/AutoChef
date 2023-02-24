60cc2e9caea6e02b25ad0456176eb37314a46f9d
cuvar <luca-mueller@gmx.net>
2023-02-24 18:48:54 +0100

    feat: check step number >= 0

--- src/main/java/de/cunc/autochef/domain/entities/RecipeStep.java
+++ src/main/java/de/cunc/autochef/domain/entities/RecipeStep.java
@@ -2,13 +2,16 @@ package de.cunc.autochef.domain.entities;
 
 public class RecipeStep {
 
-  int step;
+  int number;
   GroceryList ingredients;
   String description;
 
-  public RecipeStep(int step, String description, GroceryItem... ingredients) {
-    // todo: check step > 0
-    this.step = step;
+  public RecipeStep(int number, String description, GroceryItem... ingredients) {
+    if (number < 0) {
+      throw new IllegalArgumentException("number must be greater or equal to 0");
+    }
+
+    this.number = number;
     this.ingredients = new GroceryList(ingredients);
     this.description = description;
   }
