cb21baa70db06579e420c9582178c2b60e956433
cuvar <luca-mueller@gmx.net>
2023-02-24 18:46:41 +0100

    feat: unit with additional attributes

--- src/main/java/de/cunc/autochef/domain/valueobjects/Unit.java
+++ src/main/java/de/cunc/autochef/domain/valueobjects/Unit.java
@@ -2,12 +2,29 @@ package de.cunc.autochef.domain.valueobjects;
 
 public enum Unit {
 
-    LITER,
-    MILLILITER,
-    KILOGRAM,
-    GRAM,
-    PIECE,
-    TABLESPOON,
-    TEESPOON
+    LITER("Liter", "l"),
+    MILLILITER("Milliliter", "ml"),
+    KILOGRAM("Kilogram", "kg"),
+    GRAM("Gram", "g"),
+    PIECE("Piece", "piece(s)"),
+    TABLESPOON("Tablespoon", "tbsp"),
+    TEESPOON("Teespoon", "tsp");
+
+    private String name;
+    private String abbreviation;
+
+    Unit(String name, String abbreviation) {
+        this.name = name;
+        this.abbreviation = abbreviation;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    public String abbreviated() {
+        return this.abbreviation;
+    }
 
 }
