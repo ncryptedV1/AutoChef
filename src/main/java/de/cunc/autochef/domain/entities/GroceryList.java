27ed949ac95fc8f14c7c602fb462b817ff0985b9
ncryptedV1 <15632467+ncryptedV1@users.noreply.github.com>
2023-02-27 00:05:24 +0100

    refactor: code quality

--- src/main/java/de/cunc/autochef/domain/entities/GroceryList.java
+++ src/main/java/de/cunc/autochef/domain/entities/GroceryList.java
@@ -1,21 +1,18 @@
 package de.cunc.autochef.domain.entities;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class GroceryList {
 
-  List<GroceryItem> entries;
+  List<GroceryItem> entries = new ArrayList<>();
 
   public GroceryList() {
-    this.entries = new ArrayList<GroceryItem>();
   }
 
   public GroceryList(GroceryItem... items) {
-    this.entries = new ArrayList<GroceryItem>();
-    for (GroceryItem item : items) {
-      this.entries.add(item);
-    }
+    Collections.addAll(this.entries, items);
   }
 
   public void addItem(GroceryItem entry) {
