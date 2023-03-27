6228366d06db797f320fa91cc64d2184a86f1037
ncryptedV1 <15632467+ncryptedV1@users.noreply.github.com>
2023-03-27 02:53:40 +0200

    remove: MockService

--- src/main/java/de/cunc/autochef/AutoChef.java
+++ src/main/java/de/cunc/autochef/AutoChef.java
@@ -1,34 +1,13 @@
 package de.cunc.autochef;
 
-import de.cunc.autochef.domain.valueobjects.GroceryItem;
-import de.cunc.autochef.domain.entities.Recipe;
-import de.cunc.autochef.domain.valueobjects.RecipeStep;
-import de.cunc.autochef.domain.service.ChefkochRecipeService;
 import de.cunc.autochef.domain.service.ConsoleOutputService;
 import de.cunc.autochef.domain.service.DialogService;
-import de.cunc.autochef.domain.service.MockService;
-import de.cunc.autochef.domain.service.PersistenceService;
-import java.util.List;
 
 public class AutoChef {
 
   public static void main(String[] args) {
     ConsoleOutputService.info("Starte...");
-
-    // generate mock recipe and save for later usage
-    List<GroceryItem> groceryItems = MockService.generateGroceryItems();
-    List<RecipeStep> recipeSteps = MockService.generateRecipeSteps();
-    Recipe recipe = MockService.generateRecipe(groceryItems, recipeSteps);
-    PersistenceService.saveRecipe(recipe);
-
-    // test chefkoch recipe service
-    Recipe ckRecipe = ChefkochRecipeService.getRecipe(
-        "https://www.chefkoch.de/rezepte/1012931206519137/Milchschnitte-oder-Maxiking-Schnitte.html");
-    PersistenceService.saveRecipe(ckRecipe);
-
-    // start actual user interaction
     DialogService.startDialog();
-
     ConsoleOutputService.info("Dialog Endstatus: " + DialogService.getCurrentState());
   }
 }
